c74a01dce005a83e8e4bcde125bc1cfb44e017d3
gabriel silva <gabriel.spaulo@eaportal.org>
2025-04-04 12:13:18 -0300

    comitando

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import './Header.css' //importar o arquivo css 
+
+function Header() {
+    return (
+        <>
+            <header>
+                <img className="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM61xZUa-OTCFT_bkp5dKZ-lG-M6gFbxlP4g&s" />
+                <p className="text">Boston Celtics</p>
+            </header>
+        </>
+
+    )
+
+}
+export default Header
