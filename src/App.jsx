ed693ebbe949697494e1930005e8fec0c6808a5c
gabriel silva <gabriel.spaulo@eaportal.org>
2025-04-21 15:10:50 -0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Header from './components/Header'
 import Background from './components/background'
 import Footer from './components/Footer'
+import Card from './components/Card'
 
 
 function App() {
@@ -14,128 +15,7 @@ function App() {
         // <!--abcdefghijk-->
         <>  <Header />
             <Background></Background>
-            <div className="container">
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://images.tcdn.com.br/img/img_prod/311840/camiseta_mitchell_e_ness_nba_boston_celtics_preto_56199_4_20210806223429.jpg" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Jaylen<br /> Brown</p>
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://s2-ge.glbimg.com/0vrYoly2u3EnrgRi9RzYDcHFWxw=/0x0:1024x683/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2024/Q/a/MwVmBGSTmmCRLCLj8JAw/gettyimages-2182500383.jpg" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Jason<br />Tatum</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://cdn.nba.com/manage/2024/01/jrue-holiday-iso-look.jpg" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Jrue<br /> Holiday</p>
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTElgKaPPmDBwBrMxyqMrAkVmLS82gKU7R26A&s" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Al-Hoford</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://cdn.vox-cdn.com/thumbor/_vKidFoxsw2-FwRNiVB-GzUMepE=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22497697/usa_today_15975306.jpg" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Payton<br />Pritchard</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://jumperbrasil.com.br/wp-content/uploads/2024/06/Derrick_White-3.jpg" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Derrick<br />White</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://jumperbrasil.com.br/wp-content/uploads/2024/09/0-38.webp" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Kristaps<br />Porziņģis</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://mundonegro.inf.br/wp-content/uploads/2023/07/capa-56.jpg" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Jalem<br />bronw</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-img">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC_0sImBR_KkQAdT0OlszyICcaswIlqT2Xjw&s" alt="" />
-                </div>
-
-                <div className="absolute">
-                    <p className="points">20.8<br />Pts</p>
-                </div>
-
-                <div className="card-text">
-                    <p>Xavier<br />Tillman</p>
-                </div>
-            </div>
-        </div>
-
+            <Card></Card>
             <Footer></Footer>
 
         </>
